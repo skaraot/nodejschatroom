ac387f8f704b5ada4ef551d411358e3810a78247
skaraot <serkankaraot@hotmail.com>
2017-11-17 16:01:02 +0200

    Scroll to bottom in chat box in angularjs

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,7 +1,23 @@
 
 var chatroom = angular.module('chatroom', []);
 
-chatroom.controller ('mainController', function($scope, $http, $window, $location){
+chatroom
+.directive('schrollBottom', function () {
+  return {
+    scope: {
+      schrollBottom: "="
+    },
+    link: function (scope, element) {
+      scope.$watchCollection('schrollBottom', function (newValue) {
+        if (newValue)
+        {
+          $(element).scrollTop($(element)[0].scrollHeight);
+        }
+      });
+    }
+  }
+})
+.controller ('mainController', function($scope, $http, $window, $location){
 	
 	$relatedURL = 'http://127.0.0.1:3000/';
 	$session = false;
@@ -34,7 +50,6 @@ chatroom.controller ('mainController', function($scope, $http, $window, $locatio
 	$scope.LogOut = function(){
 		$window.location.href = $relatedURL + 'logout';
 	}
-
 
 	var getContact = function(){
 		$scope.contact = [];
@@ -74,7 +89,13 @@ chatroom.controller ('mainController', function($scope, $http, $window, $locatio
 			url: $relatedURL+'messageAdd?from='+$nickname+'&to='+$toSource+'&message='+$scope.messageText
 		}).then(
 			function successCallback(response){
-				$scope.getMessage($toSource);
+				$scope.message.push({
+			      "from": $nickname,
+			      "to": $toSource,
+			      "time": Math.floor(Date.now() / 1000),
+			      "message": $scope.messageText
+			    });
+				$scope.messageText = '';
 			},
 			function errorCallback(response){
 				console.log('Error: ' + response);
